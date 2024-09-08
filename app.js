d52a4436d754ec788bf1bca7872a9350bcbc3fdc
cakirhalil <halilcakir343@gmail.com>
2024-09-08 13:38:49 +0300

    Partials, public ve views eklendi ve düzenlemeler yapıldı

--- app.js
+++ app.js
@@ -3,10 +3,20 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+//ejs template engine
+app.set('view engine', 'ejs');
+
+//static files middleware
+app.use(express.static('public'));
+
 app.listen(port, () => {
     console.log(`Application running on port: ${port}`);
   });
 
 app.get('/', (req, res) => {
-    res.send('index sayfası');
+    res.render('index');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
 });
