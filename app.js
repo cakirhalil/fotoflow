7418b404bf2de1c5d991b0e6a90a22b36aff52c1
cakirhalil <halilcakir343@gmail.com>
2024-09-10 21:08:46 +0300

    mongoose and schema

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 import conn from './db.js';
 import pageRoute from './routes/pageRoute.js';
+import photoRoute from './routes/photoRoute.js';
+
 
 dotenv.config();
 
@@ -18,8 +20,9 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 //routes
-
 app.use('/', pageRoute);
+app.use('/photo', photoRoute);
+
 
 
 app.listen(port, () => {
