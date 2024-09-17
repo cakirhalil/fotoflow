0e5c6e1828ede2020e95b0741f787c23ee30c2c1
cakirhalil <halilcakir343@gmail.com>
2024-09-17 10:54:42 +0300

    Takipçi - Takip Edilen Metodları

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -16,7 +16,12 @@ router
 router
     .route('/:id')
     .get(authMiddleware.authenticateToken, userController.getAUser);    
-
+router
+    .route('/:id/follow')
+    .put(authMiddleware.authenticateToken, userController.follow);
+router
+    .route('/:id/unfollow')
+    .put(authMiddleware.authenticateToken, userController.unfollow);
 
 
 export default router; 
